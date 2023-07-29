2d5031c9e1c235b8bab46e58fb3cec2e39dd0d83
Baranidharan <baranidharan.balamurugan@buildsuite.io>
2023-07-29 14:41:58 +0530

    minor

--- index.js
+++ index.js
@@ -52,14 +52,49 @@ const fetchPrice = (productUrl, price) => {
             console.log(currentPrice, price, strPrice, strPrice.split(',').join(""));
 
             if (currentPrice == price) {
-                console.log("Equal Price")
+                console.log("Equal Price");
+                sendNotification(productUrl);
             } else if (currentPrice > price) {
                 console.log("Wait for price to decrease");
             } else if (currentPrice < price) {
                 console.log("Its time to buy your product");
+                sendNotification(productUrl);
             }
         })
     })
+}
+
+
+function sendNotification(productUrl) {
+    const receivedToken = req.body.fcmToken;
+
+    console.log(receivedToken);
+
+    const message = {
+        notification: {
+            title: "Price alert",
+            body: "It's time to order"
+        },
+        data: {
+            test: productUrl
+        },
+        token: "ftTEe1fVTP2_xzGtwRToH4:APA91bGpgViNM4oFCxVQShEt5gZ7H1E4vvipZBK818SVwv7RF1eb8q5HBjktYWNdAyRmSapsPTTaV6ZDmyNQxcfXPODk0E9x3IrOcUoFtVv26XQXrIYjjTS9DTzrh8ftFQyzfD7xLBMV",
+    };
+
+    getMessaging()
+        .send(message)
+        .then((response) => {
+            res.status(200).json({
+                message: "Successfully sent message",
+                token: receivedToken,
+            });
+            console.log("Successfully sent message:", response);
+        })
+        .catch((error) => {
+            res.status(400);
+            res.send(error);
+            console.log("Error sending message:", error);
+        });
 }
 
 // Task schema and model
