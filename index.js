298f14c5f2d3effb2b0a5a9d184904524bd072e0
Baranidharan <baranidharan.balamurugan@buildsuite.io>
2023-07-29 15:15:04 +0530

    final

--- index.js
+++ index.js
@@ -78,21 +78,15 @@ function sendNotification(productUrl) {
         data: {
             test: productUrl
         },
-        token: "ftTEe1fVTP2_xzGtwRToH4:ftTEe1fVTP2_xzGtwRToH4:APA91bGpgViNM4oFCxVQShEt5gZ7H1E4vvipZBK818SVwv7RF1eb8q5HBjktYWNdAyRmSapsPTTaV6ZDmyNQxcfXPODk0E9x3IrOcUoFtVv26XQXrIYjjTS9DTzrh8ftFQyzfD7xLBMV",
+        token: "ftTEe1fVTP2_xzGtwRToH4:APA91bGpgViNM4oFCxVQShEt5gZ7H1E4vvipZBK818SVwv7RF1eb8q5HBjktYWNdAyRmSapsPTTaV6ZDmyNQxcfXPODk0E9x3IrOcUoFtVv26XQXrIYjjTS9DTzrh8ftFQyzfD7xLBMV",
     };
 
     getMessaging()
         .send(message)
         .then((response) => {
-            // res.status(200).json({
-            //     message: "Successfully sent message",
-            //     token: "ftTEe1fVTP2_xzGtwRToH4:ftTEe1fVTP2_xzGtwRToH4:APA91bGpgViNM4oFCxVQShEt5gZ7H1E4vvipZBK818SVwv7RF1eb8q5HBjktYWNdAyRmSapsPTTaV6ZDmyNQxcfXPODk0E9x3IrOcUoFtVv26XQXrIYjjTS9DTzrh8ftFQyzfD7xLBMV",
-            // });
             console.log("Successfully sent message:", response);
         })
         .catch((error) => {
-            // res.status(400);
-            // res.send(error);
             console.log("Error sending message:", error);
         });
 }
@@ -149,15 +143,15 @@ app.post("/send", function (req, res) {
     getMessaging()
         .send(message)
         .then((response) => {
-            // res.status(200).json({
-            //     message: "Successfully sent message",
-            //     token: receivedToken,
-            // });
+            res.status(200).json({
+                message: "Successfully sent message",
+                token: receivedToken,
+            });
             console.log("Successfully sent message:", response);
         })
         .catch((error) => {
-            // res.status(400);
-            // res.send(error);
+            res.status(400);
+            res.send(error);
             console.log("Error sending message:", error);
         });
 
